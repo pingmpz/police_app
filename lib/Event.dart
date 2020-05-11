3d5dc9fa0b402b2628b6351ee0cccae457c9f6b2
pingmpz <pingmpz@hotmail.com>
2020-05-11 08:16:11 +0700

    Just Coding

---
+++ lib/Event.dart
@@ -0,0 +1,6 @@
+class Event {
+  int id;
+  String title;
+  String subtitle;
+  Event(this.id, this.title, this.subtitle);
+}
--- lib/eventlistpage.dart
+++ lib/eventlistpage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'Event.dart';
+
 class MyEventListPage extends StatefulWidget {
   @override
   _MyEventListPageState createState() => _MyEventListPageState();
@@ -10,6 +12,40 @@ class _MyEventListPageState extends State<MyEventListPage> {
   Color icon1Color = Colors.white;
   Color appBar2Color = Color(0xFFEFEFEF);
   Color icon2Color = Color(0xFF263147);
+
+  // !- Test Data
+  List<Event> eventList = new List();
+  List<Widget> cards = new List();
+
+  @override
+  void initState() {
+    testData(); // !- For Test Only
+    setEventList();
+    super.initState();
+  }
+
+  // !- Test Data
+  void testData(){
+    eventList.add(new Event(1, 'Event #1', 'MON 07 AUG 2020 19.04'));
+    eventList.add(new Event(2, 'Event #2', 'MON 07 AUG 2020 19.04'));
+    eventList.add(new Event(3, 'Event #3', 'MON 07 AUG 2020 19.04'));
+  }
+
+  void setEventList() {
+      cards.clear();
+      for (int i = 0; i < eventList.length; i++) {
+        cards.add(
+          Card(
+            child: ListTile(
+                title: Text(eventList[i].title),
+                subtitle: Text(eventList[i].subtitle),
+                onTap: () {
+                  // !- F
+                }),
+          ),
+        );
+      }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,17 +69,7 @@ class _MyEventListPageState extends State<MyEventListPage> {
         ),
         body: Center(
           child: ListView(
-            children: <Widget>[
-              Card(
-                child: ListTile(
-                  title: Text('Title'),
-                  subtitle: Text('Subtitle'),
-                  onTap: () {
-                    // !- F
-                  },
-                ),
-              ),
-            ],
+            children: cards,
           ),
         ),
       ),
